afdaab34aa8bca80a0eca551fcaf90b0c209d219
Edson Alexandre Sensato <edson.sensato@espm.br>
2021-09-22 20:36:48 -0300

    Aula 20/09

--- VenCarro/app/src/main/java/com/example/vencarro/ListaVeiculoFragment.kt
+++ VenCarro/app/src/main/java/com/example/vencarro/ListaVeiculoFragment.kt
@@ -1,11 +1,17 @@
 package com.example.vencarro
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.vencarro.databinding.FragmentListaVeiculoBinding
+import com.example.vencarro.databinding.LinhaListaBinding
+import org.json.JSONArray
 
 class ListaVeiculoFragment : Fragment() {
 
@@ -16,6 +22,12 @@ class ListaVeiculoFragment : Fragment() {
 
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        binding.listaVeiculo.layoutManager = LinearLayoutManager(context!!)
+        binding.listaVeiculo.adapter = VeiculoAdapter(context!!)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -24,5 +36,72 @@ class ListaVeiculoFragment : Fragment() {
         binding = FragmentListaVeiculoBinding.inflate(inflater, container, false)
         return binding.root
     }
+
+    class VeiculoHolder(itemLista:LinhaListaBinding):RecyclerView.ViewHolder(itemLista.root){
+
+        var txtMarca = itemLista.txtMarca
+        var txtModelo = itemLista.txtModelo
+        var txtAno = itemLista.txtAno
+        var item = itemLista.item
+
+    }
+
+    class VeiculoAdapter(context: Context):RecyclerView.Adapter<VeiculoHolder>(){
+
+        var veiculos = ArrayList<Veiculo>()
+
+        var queue:RequestQueue
+
+        init {
+            //veiculos.add(Veiculo(marca="Ford", modelo="Ka", ano="2020"))
+            //veiculos.add(Veiculo(marca="Fiat", modelo="Uno", ano="2021"))
+            queue = Volley.newRequestQueue(context)
+            obterMarcas()
+        }
+
+        fun obterMarcas() {
+
+            queue.add(JsonArrayRequest(
+                Request.Method.GET,
+                "https://parallelum.com.br/fipe/api/v1/carros/marcas",
+                null,
+                Response.Listener<JSONArray>{ response ->
+                    // [{"nome":"Acura","codigo":"1"},{"nome":"Agrale","codigo":"2"},{"nome":"Alfa Romeo","codigo":"3"}...
+                    for (i in 0 until response.length()) {
+                        val obj = response.getJSONObject(i)
+                        veiculos.add(Veiculo(obj.getInt("codigo"), obj.getString("nome"), 0, "",0, ""))
+                    }
+                    notifyDataSetChanged()
+                },
+                Response.ErrorListener {
+                        error -> Log.e("CARRO", "ERRO: " + error.message)
+                }
+            )
+
+            )
+
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VeiculoHolder {
+            val layout = LinhaListaBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            return VeiculoHolder(layout)
+        }
+
+        override fun onBindViewHolder(holder: VeiculoHolder, position: Int) {
+            holder.txtMarca.text = veiculos.get(position).marca
+            holder.txtModelo.text = veiculos.get(position).modelo
+            holder.txtAno.text = veiculos.get(position).ano
+            holder.item.setOnClickListener {
+                Log.i("Veiculo", veiculos.get(position).marca)
+            }
+
+        }
+
+        override fun getItemCount(): Int {
+            return veiculos.size
+        }
+
+    }
+
 
 }
---
+++ VenCarro/app/src/main/java/com/example/vencarro/Veiculo.kt
@@ -0,0 +1,6 @@
+package com.example.vencarro
+
+data class Veiculo( var idMarca:Int = 0, var marca:String = "",
+                    var idModelo:Int = 0, var modelo:String = "",
+                    var idAno:Int = 0, var ano:String = "",
+                    var preco:String = "")
