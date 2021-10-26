8a1d5546886eb4f3996156b7f4deeedfe7d45a68
Edson Alexandre Sensato <edson.sensato@espm.br>
2021-10-25 21:32:21 -0300

    Aula 25/10

--- alo_flutter/lib/app.dart
+++ alo_flutter/lib/app.dart
@@ -1,8 +1,21 @@
+import 'package:alo_flutter/item_menu.dart';
 import 'package:flutter/material.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+
+  @override
+  State<App> createState() => _AppState();
+
+}
+
+class _AppState extends State<App> {
 
   String msg = 'Boa noite!';
+  String hora = '12:00';
+  int contador = 0;
+
+   final listaItensMenu = [ItemMenu(titulo: "Foto", icone: Icon(Icons.camera_alt, color: Colors.blue,)),
+     ItemMenu(titulo: "Sair", icone: Icon(Icons.transit_enterexit, color: Colors.blue,))];
 
   @override
   Widget build(BuildContext context) {
@@ -11,21 +24,66 @@ class App extends StatelessWidget {
                        home: Scaffold(
                          appBar: AppBar(
                            title: Text('Titulo'),
+                           leading: PopupMenuButton<ItemMenu>(
+                           itemBuilder: (BuildContext ctx) {
+                             return listaItensMenu.map((e) {
+                               return PopupMenuItem<ItemMenu>(child: Row(children: [
+                                 e.icone,
+                                 Text(e.titulo)
+                               ],
+                               )
+                               );
+                             }).toList();
+                            },
+                           )
                          ),
-                         body: Center(child: Text(this.msg),),
-                         bottomNavigationBar: BottomAppBar(child: Row(children: [
-                           Text('Aqui embaixo!'),
-                           Icon(Icons.adb_outlined)
-                         ],)),
+                         body: Center(child: Row(mainAxisAlignment: MainAxisAlignment.center,
+                             children:[
+                           IconButton(onPressed: (){
+                             setState(() {
+                               this.contador++;
+                             });
+                           }, icon: Icon(Icons.upload_sharp)),
+                           Text('${this.contador}',
+                             style: TextStyle(color: Colors.deepOrange, fontSize: 30)
+                         ),
+                           IconButton(onPressed: (){
+                             setState(() {
+                               this.contador--;
+                             });
+                           }, icon: Icon(Icons.download_sharp)),
+                         ]),
+
+                         ),
+                         bottomNavigationBar: BotaoPersonalizado(),
                          floatingActionButton: FloatingActionButton(
                            child: Icon(Icons.water_damage_outlined),
                            onPressed: () {
-                             this.msg = 'Bom dia';
+                             setState(() {
+                               this.hora = "15:00";
+                               this.msg = 'Bom dia';
+                             });
                              print('Clicou!!!!');
                            },
                          ),
+                         persistentFooterButtons: [IconButton(onPressed: () {
+                           print ('Clicou!');
+                         }, icon: Icon(Icons.access_time))],
                        ),
     );
   }
 
+}
+
+class BotaoPersonalizado extends StatelessWidget {
+
+  final String hora = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(child: Row(children: [
+      Text(this.hora),
+      Icon(Icons.adb_outlined)
+    ],));
+  }
 }
---
+++ alo_flutter/lib/item_menu.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ItemMenu {
+
+  final String titulo;
+  final Icon icone;
+
+  ItemMenu({this.titulo = '', this.icone = const Icon(Icons.upload_sharp)});
+
+}
